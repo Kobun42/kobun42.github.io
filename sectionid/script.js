8146e4f6c82ed153e6fb29337e9724aca0132998
Witty Name goes Here <37277630+Kobun42@users.noreply.github.com>
2022-09-01 12:06:24 -0400

    begin script

---
+++ sectionid/script.js
@@ -0,0 +1,28 @@
+// Section ID calculator script portion
+var classOffsetBB = { // Class offset stuff for Blue Burst
+  "HUdiv": "5",
+  
+  "HUmar": "5",
+  "HUnewearl": "6",
+  "HUcast": "7",
+  "HUcaseal": "4",
+  
+  "RAdiv": "8",
+  
+  "RAmar": "8",
+  "RAmarl": "6",
+  "RAcast": "9",
+  "RAcaseal": "0",
+  
+  "FOdiv": "5",
+  
+  "FOmar": "5",
+  "FOmarl": "1",
+  "FOnewm": "2",
+  "FOnewearl": "3"
+}
+
+function calculate()
+{
+  alert("not ready yet lol");
+}
